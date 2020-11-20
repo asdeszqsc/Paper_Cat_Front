5625e21186785ac0e9cad35c4d34870a2afa4439
yunhoe <dbsghl9709@gmail.com>
2020-11-20 20:38:35 +0900

    make keyword list create function

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,41 +2,56 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../css/input.css';
 import { FormControl, InputGroup, Button } from 'react-bootstrap';
-
-
-// function SubmitInput(){
-//     function handleClick(e){
-//         e.preventDefault();
-//         console.log('The link was clicked');
-//     }
-
-//     return(
-//         <a></a>
-//     );
-// }
-
+import { Link } from 'react-router-dom';
 
 class SearchBox extends Component {
+    
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            keyword:[{
+                id:0,
+                key:'',
+                }
+            ] 
+        }
     }
-    render() { 
+
+    SubmitbyEnter = (target) => {
+        if(target.charCode == 13){ 
+            this.handleCreate();
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({keyword: event.target.value})
+    }
+
+    handleCreate = (data) =>{
+        const keyword = this.state;
+        this.setState({keyword: keyword.concat({id: this.keyword.id++, ...data})})
+    }
+
+    render() {
         return ( 
             <span className="search-bar">
-                <InputGroup className="mb-3">
-                    
+                <InputGroup className="mb-3" onKeyPress={this.SubmitbyEnter} >
                     <FormControl
                         aria-label="키워드 검색"
                         aria-describedby="basic-addon2"
+                        type="text"
+                        value={this.state.keyword.key}
+                        onChange={this.handleChange}
                     />
                     <InputGroup.Append>
-                    <Button type="submit" variant="outline-secondary" className="input-button">
-                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg" style={{marginBottom:'5px'}}>
-                        <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
-                        <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
-                        </svg>
-                    </Button>
+                        <Link to='/about'>
+                            <Button type="submit" variant="outline-secondary" className="input-button" >
+                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg" style={{marginBottom:'10px', position:'absolute', right:'8px', top:'8px'}}>
+                                <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
+                                <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+                                </svg>
+                            </Button>
+                        </Link>
                     </InputGroup.Append>
                 </InputGroup>
             </span>
